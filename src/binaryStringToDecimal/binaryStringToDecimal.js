fcaee7a297d59c684917bb0c12c199821c9de8dd
biabalestra-192 <biabalestra@gmail.com>
2025-07-16 18:18:57 -0300

    numeros binarios para decimais

--- src/binaryStringToDecimal/binaryStringToDecimal.js
+++ src/binaryStringToDecimal/binaryStringToDecimal.js
@@ -1,3 +1,16 @@
-function binaryStringToDecimal(binary) {}
+function binaryStringToDecimal(binary) {
+    let power = 0;
+    let sum = 0;
+    for (let i = binary.length - 1; i >= 0; i -= 1) {
+        if (binary[i] == 1) {
+            sum += 2 ** power;
+        }
+        power += 1;
+        // for (let j = 0; j < binary.length; j += 1) {
+        //     // sum += binary[j];
+        // }
+    }
+    return sum;
+}
 
 module.exports = { binaryStringToDecimal };
