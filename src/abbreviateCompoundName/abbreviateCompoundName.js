7979cb48422033eff9327e630d724ade7a64e5ca
biabalestra-192 <biabalestra@gmail.com>
2025-07-24 18:44:01 -0300

    abreviação de nomes

--- src/abbreviateCompoundName/abbreviateCompoundName.js
+++ src/abbreviateCompoundName/abbreviateCompoundName.js
@@ -1,5 +1,17 @@
-function abbreviateCompoundName(name) {}
+function abbreviateCompoundName(name) {
+    let array = name.split(' ');
+    if (array.length > 1) {
+        for (let i = 0; i < name.length; i += 1) {
+            let str = array[0][0] + '.' + array[array.length - 1][0];
+            return str.toUpperCase();
+        }
+    } else if (name == '') {
+        return '';
+    } else if (array.length === 1) {
+        return array[0][0];
+    }
+}
 
 module.exports = {
-    abbreviateCompoundName
+    abbreviateCompoundName,
 };
