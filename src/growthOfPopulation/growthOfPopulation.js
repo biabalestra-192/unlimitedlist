50594ccdf733f316335661ff6f2aac349c63d1a8
biabalestra-192 <biabalestra@gmail.com>
2025-07-24 17:39:03 -0300

    crescimento da população

--- src/growthOfPopulation/growthOfPopulation.js
+++ src/growthOfPopulation/growthOfPopulation.js
@@ -3,7 +3,15 @@ function growthOfPopulation(
     growthRate,
     newPeople,
     targetPopulation,
-) {}
+) {
+    let n = 0;
+    let final = initialPopulation;
+    while (final < targetPopulation) {
+        final = final + final * (growthRate / 100) + newPeople;
+        n += 1;
+    }
+    return n;
+}
 
 module.exports = {
     growthOfPopulation,
