3dd39f3008ab09f0550876ee825f2332f9c84650
biabalestra-192 <biabalestra@gmail.com>
2025-07-30 18:36:12 -0300

    sequencia de tribonacci

--- src/tribonacciSequence/tribonacciSequence.js
+++ src/tribonacciSequence/tribonacciSequence.js
@@ -1,6 +1,20 @@
 // Enunciado: https://www.codewars.com/kata/556deca17c58da83c00002db/javascript
-function tribonacciSequence(signature, n) {}
+function tribonacciSequence(signature, n) {
+    if (n === 0) {
+        return [];
+    }
+    if (n === 1) {
+        return [signature[0]];
+    }
+    if (n === 2) {
+        return [signature[0], signature[1]];
+    }
+    for (let i = 3; i < n; i++) {
+        signature.push(signature[i - 1] + signature[i - 2] + signature[i - 3]);
+    }
+    return signature;
+}
 
 module.exports = {
     tribonacciSequence,
-}
+};
