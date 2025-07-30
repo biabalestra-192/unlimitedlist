e3115efba4d20c78f1fd8ad2f4ecb470c2297443
biabalestra-192 <biabalestra@gmail.com>
2025-07-30 18:59:25 -0300

    area e perimetro

--- src/areaOrPerimeter/areaOrPerimeter.js
+++ src/areaOrPerimeter/areaOrPerimeter.js
@@ -1,6 +1,11 @@
 // Enunciado: https://www.codewars.com/kata/5ab6538b379d20ad880000ab/javascript
-function areaOrPerimeter(height, width) {}
-
+function areaOrPerimeter(height, width) {
+    if (height === width) {
+        return height * width;
+    } else {
+        return 2 * (height + width);
+    }
+}
 module.exports = {
     areaOrPerimeter,
 };
